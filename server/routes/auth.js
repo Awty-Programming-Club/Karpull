7a0f9301ed5706557fdf890bdd8bb66e40ef83ff
Terence Dumas <dumast@awty.org>
2022-02-05 20:50:25 -0600

    distance calculation between puller and pulled

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,9 +5,9 @@ const router = express.Router()
 router.post("/create-user", actions.addNew)
 router.post("/signin", actions.signIn)
 
-router.get("/getinfo", actions.AuthCheck, (req, res) => {
-    console.log("HELLO", req.user)
-    res.sendStatus(200)
-})
+// router.get("/getinfo", actions.AuthCheck, (req, res) => {
+//     console.log("HELLO", req.user)
+//     res.sendStatus(200)
+// })
 
 module.exports = router
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,30 @@ const actions = require('../methods/actions')
 const router = express.Router()
 const User = require('../models/user')
 
+function distanceBetweenCoordinates(coordinate1, coordinate2){
+    lat1 = coordinate1.latitude
+    lon1 = coordinate1.longitude
+    lat2 = coordinate2.latitude
+    lon2 = coordinate2.longitude
+
+    function toRad(x) {
+        return x * Math.PI / 180;
+    }
+
+    var R = 6371; // km
+
+    var x1 = lat2 - lat1;
+    var dLat = toRad(x1);
+    var x2 = lon2 - lon1;
+    var dLon = toRad(x2)
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d * 1000
+}
+
 router.post("/update/location", async (req, res) => {
     const checkuser = actions.AuthCheck(req)
     if(!checkuser) return res.sendStatus(403)
@@ -17,12 +41,20 @@ router.post("/update/location", async (req, res) => {
     }
 
     if(!user.partner) return res.sendStatus(406)
-
     if(user.puller === false) return res.sendStatus(200)
+    const pulledUser = await User.findById(user.partner)
 
+    const pullerCoordinates = {
+        latitude: user.latitude,
+        longitude: user.longitude
+    }
+    const pulledCoordinates = {
+        latitude: pulledUser.latitude,
+        longitude: pulledUser.longitude
+    }
 
-
-    res.sendStatus(200)
+    const distance = distanceBetweenCoordinates(pullerCoordinates, pulledCoordinates)
+    return res.status(200).json({here: (distance<=50), distance: distance})
 })
 
 module.exports = router
