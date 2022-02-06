e5d89fc5f5875b5e27a201c3c596dfbfb4397b57
AshamedConsequence <60143430+AshamedConsequence@users.noreply.github.com>
2022-02-06 15:09:03 -0600

    ui changes last minute

--- app/lib/screens/mainscreen.dart
+++ app/lib/screens/mainscreen.dart
@@ -62,86 +62,110 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     getUser();
     return Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
-            Widget>[
-      if (user == null || (user['partner'] == null && (user['puller'] == true)))
-        Center(
-          child: (Column(children: [
-            TextField(
-                decoration: InputDecoration(labelText: 'Partner Username'),
-                onChanged: (val) {
-                  setState(() {
-                    partnerUsername = val;
-                  });
-                }),
-            RaisedButton(
-              child: Text("Submit"),
-              onPressed: () async {
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                String? token = prefs.getString('token');
-                await AuthService().setPartner(partnerUsername, token, context);
-              },
-            )
-          ])),
-        ),
-      if (user != null && user['partner'] != null)
-        Center(
-          child: (Column(children: [
-            Container(
-              margin: EdgeInsets.all(5),
-              child: Container(
-                child: here
-                    ? Center(
-                        child: Text(
-                          "Here",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 21, 20, 1),
-                            fontSize: 30,
-                          ),
-                        ),
-                      )
-                    : Center(
-                        child: Text(
-                          "Approaching",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 199, 0),
-                            fontSize: 30,
-                          ),
-                        ),
-                      ),
-                width: MediaQuery.of(context).size.width * 0.6,
-                height: MediaQuery.of(context).size.height * 0.6,
-                decoration: new BoxDecoration(
-                  color: here
-                      ? Color.fromARGB(255, 255, 199, 0)
-                      : Color.fromARGB(255, 21, 20, 1),
-                  boxShadow: [
-                    BoxShadow(
-                      color:
-                          Color.fromARGB(255, 255, 212, 58), //color of shadow
-                      spreadRadius: 5, //spread radius
-                      blurRadius: 7, // blur radius
-                    )
-                  ],
-                  shape: BoxShape.circle,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          if (user == null ||
+              (user['partner'] == null && (user['puller'] == true)))
+            Center(
+              child: (Column(children: [
+                Container(
+                  decoration:
+                      BoxDecoration(color: Color.fromARGB(255, 56, 47, 14)),
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Partner Username',
+                          border: OutlineInputBorder(),
+                          labelStyle: TextStyle(
+                              color: Color.fromARGB(255, 255, 199, 0))),
+                      onChanged: (val) {
+                        setState(() {
+                          partnerUsername = val;
+                        });
+                      }),
                 ),
-              ),
+                Container(
+                  margin: EdgeInsets.all(25),
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: RaisedButton(
+                    child: Text("Submit"),
+                    color: Color.fromARGB(255, 255, 199, 0),
+                    onPressed: () async {
+                      SharedPreferences prefs =
+                          await SharedPreferences.getInstance();
+                      String? token = prefs.getString('token');
+                      await AuthService()
+                          .setPartner(partnerUsername, token, context);
+                    },
+                  ),
+                )
+              ])),
             ),
-            Container(
-                margin: EdgeInsets.all(5),
-                child: Text(distance.toString() + "m",
-                    style: TextStyle(color: Color.fromARGB(255, 255, 199, 0))))
-          ])),
-        ),
-      RaisedButton(
-          child: Text("Back"),
-          color: Colors.yellow,
-          onPressed: () {
-            // SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-            // sharedPreferences.setString("token", "");
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => LoginScreen()));
-          }),
-    ]));
+          if (user != null && user['partner'] != null)
+            Center(
+              child: (Column(children: [
+                Container(
+                  margin: EdgeInsets.all(5),
+                  child: Container(
+                    child: here
+                        ? Center(
+                            child: Text(
+                              "Here",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 21, 20, 1),
+                                fontSize: 30,
+                              ),
+                            ),
+                          )
+                        : Center(
+                            child: Text(
+                              "Approaching",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 255, 199, 0),
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    decoration: new BoxDecoration(
+                      color: here
+                          ? Color.fromARGB(255, 255, 199, 0)
+                          : Color.fromARGB(255, 21, 20, 1),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(
+                              255, 255, 212, 58), //color of shadow
+                          spreadRadius: 5, //spread radius
+                          blurRadius: 7, // blur radius
+                        )
+                      ],
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.all(5),
+                    child: Text(distance.toString() + "m",
+                        style:
+                            TextStyle(color: Color.fromARGB(255, 255, 199, 0))))
+              ])),
+            ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: RaisedButton(
+                child: Text("Back"),
+                color: Color.fromARGB(255, 255, 199, 0),
+                onPressed: () {
+                  // SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+                  // sharedPreferences.setString("token", "");
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
